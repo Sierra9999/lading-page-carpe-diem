80db15acbd3af73a1313290df379c4fee3f41228
Mateo Sierra <m.sierra1037@hotmail.com>
2023-10-02 20:20:37 -0600

    assets added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main>
       <section className='division hero-section'>
-      <section className='hero-cta'>
+        <section className='hero-cta'>
           <h1>Carpe Diem</h1>
           <h2>Your perfect journey, made with care and personal attention</h2>
           <h3>Be the first to know about our deals <br />
@@ -14,6 +14,30 @@ export default function Home() {
           <button>Start Journey</button>
         </section>
         <Image src={'/hero.jpeg'} alt='hero' width={900} height={500}></Image>
+      </section>
+      <section className='division' id='about-us'>
+        <h3>about us</h3>
+        <h1>we are individuals as should be our vacation and services</h1>
+
+        <section className="services">
+        <div className="service-card">
+          <h2 className="service">Original route</h2>
+          <p className="description">Travel to the most interesting and unique places with local guides</p>
+        </div>
+        <div className="service-card">
+          <h2 className="service">Original route</h2>
+          <p className="description">Travel to the most interesting and unique places with local guides</p>
+        </div>
+        <div className="service-card">
+          <h2 className="service">Original route</h2>
+          <p className="description">Travel to the most interesting and unique places with local guides</p>
+        </div>
+        <div className="service-card">
+          <h2 className="service">Original route</h2>
+          <p className="description">Travel to the most interesting and unique places with local guides</p>
+        </div>
+
+        </section>
       </section>
     </main>
   )
