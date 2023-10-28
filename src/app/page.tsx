882c7dcfc08812958e809400fc0803ea4a076ab3
Mateo Sierra <m.sierra1037@hotmail.com>
2023-10-27 18:34:00 -0600

    back into coding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,38 +4,43 @@ import './page.css'
 export default function Home() {
   return (
     <main>
-      <section className='division hero-section'>
+      <section className='hero division'>
         <section className='hero-cta'>
-          <h1>Carpe Diem</h1>
-          <h2>Your perfect journey, made with care and personal attention</h2>
-          <h3>Be the first to know about our deals <br />
-            sign up for our news letter</h3>
-          <input type="text" placeholder='your-email@email.com' />
-          <button>Start Journey</button>
+          <h1 className='hero-cta-title'>Carpe Diem</h1>
+          <h2 className='hero-cta-description'>Your perfect journey, made with care and personal attention</h2>
+          <section className='newsletter'>
+            <h3>Be the first to know about our deals <br />
+              sign up for our news letter</h3>
+            <form className='newsletter-form' action="">
+              <input type="email" placeholder='your-email@email.com' />
+              <button>Start Journey</button>
+            </form>
+
+          </section>
+
         </section>
-        <Image src={'/hero.jpeg'} alt='hero' width={900} height={500}></Image>
       </section>
       <section className='division' id='about-us'>
         <h3>about us</h3>
         <h1>we are individuals as should be our vacation and services</h1>
 
         <section className="services">
-        <div className="service-card">
-          <h2 className="service">Original route</h2>
-          <p className="description">Travel to the most interesting and unique places with local guides</p>
-        </div>
-        <div className="service-card">
-          <h2 className="service">Original route</h2>
-          <p className="description">Travel to the most interesting and unique places with local guides</p>
-        </div>
-        <div className="service-card">
-          <h2 className="service">Original route</h2>
-          <p className="description">Travel to the most interesting and unique places with local guides</p>
-        </div>
-        <div className="service-card">
-          <h2 className="service">Original route</h2>
-          <p className="description">Travel to the most interesting and unique places with local guides</p>
-        </div>
+          <div className="service-card">
+            <h2 className="service">Original route</h2>
+            <p className="description">Travel to the most interesting and unique places with local guides</p>
+          </div>
+          <div className="service-card">
+            <h2 className="service">Original route</h2>
+            <p className="description">Travel to the most interesting and unique places with local guides</p>
+          </div>
+          <div className="service-card">
+            <h2 className="service">Original route</h2>
+            <p className="description">Travel to the most interesting and unique places with local guides</p>
+          </div>
+          <div className="service-card">
+            <h2 className="service">Original route</h2>
+            <p className="description">Travel to the most interesting and unique places with local guides</p>
+          </div>
 
         </section>
       </section>
